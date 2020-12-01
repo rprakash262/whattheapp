65d68aec09d77b05d4e9b2a2445534ef6824edc5
ravi <rprakash262@gmail.com>
2020-12-01 20:42:59 +0530

    change ENDPOINT for production

--- client/src/reducers/LayoutReducer.js
+++ client/src/reducers/LayoutReducer.js
@@ -11,8 +11,8 @@ import {
 import { getUserCookie, deleteCookie } from '../cookie';
 import { setCurrentUser } from './LoginReducer';
 
-const ENDPOINT = "http://localhost:5000";
-// const ENDPOINT = 'https://arcane-wildwood-43524.herokuapp.com';
+// const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = 'https://arcane-wildwood-43524.herokuapp.com';
 const socket = socketIOClient(ENDPOINT);
 
 const INIT = 'LayoutReducer/INIT';
